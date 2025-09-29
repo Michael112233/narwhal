2bfe716632c57eb069bcf32ad7f1024480d8f76f
Michael112233 <88572680+Michael112233@users.noreply.github.com>
2025-09-29 22:32:21 +0800

    Update fabfile.py

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -14,7 +14,7 @@ def local(ctx, debug=True):
     ''' Run benchmarks on localhost '''
     bench_params = {
         'faults': 0,
-        'nodes': 4,
+        'nodes': 7,
         'workers': 1,
         'rate': 50_000,
         'tx_size': 512,
@@ -37,7 +37,7 @@ def local(ctx, debug=True):
 
 
 @task
-def create(ctx, nodes=2):
+def create(ctx, nodes=4):
     ''' Create a testbed'''
     try:
         InstanceManager.make().create_instances(nodes)
