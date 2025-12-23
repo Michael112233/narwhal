5fb39dbba1316c5a3feec250d13a2be9460e1250
chenlinwu2 <michaelwcl@cityu.edu.hk>
2025-12-23 20:04:35 +0800

    update code sync

--- script/remote_control/codebase_update.sh
+++ script/remote_control/codebase_update.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/Michael112233/narwhal.git -b experiment1
 cd narwhal || (echo "Repository narwhal not found. Please run: fab cloudlab-install" && exit 1)
 git fetch
 git checkout experiment1
