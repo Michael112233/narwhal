2b76a9191a72bf865b43da99a4f83ddd90ba98b1
chenlinwu2 <michaelwcl@cityu.edu.hk>
2026-03-15 21:54:36 +0800

    Discard sanitize_vote

--- primary/src/core.rs
+++ primary/src/core.rs
@@ -518,23 +518,24 @@ impl Core {
     }
 
     fn sanitize_vote(&mut self, vote: &Vote) -> DagResult<()> {
-        ensure!(
-            self.current_header.round >= vote.round,
-            DagError::TooOld(vote.digest(), vote.round)
-        );
+        // ensure!(
+        //     self.current_header.round >= vote.round,
+        //     DagError::TooOld(vote.digest(), vote.round)
+        // );
 
-        // Ensure we receive a vote on the expected header.
-        ensure!(
-            // vote.id == self.current_header.id
-            //     && vote.origin == self.current_header.author
-            //     && vote.round == self.current_header.round,
-            vote.id == self.current_header.id 
-                && vote.origin == self.current_header.author,
-            DagError::UnexpectedVote(vote.id.clone())
-        );
+        // // Ensure we receive a vote on the expected header.
+        // ensure!(
+        //     // vote.id == self.current_header.id
+        //     //     && vote.origin == self.current_header.author
+        //     //     && vote.round == self.current_header.round,
+        //     vote.id == self.current_header.id 
+        //         && vote.origin == self.current_header.author,
+        //     DagError::UnexpectedVote(vote.id.clone())
+        // );
 
-        // Verify the vote.
-        vote.verify(&self.committee).map_err(DagError::from)
+        // // Verify the vote.
+        // vote.verify(&self.committee).map_err(DagError::from)
+        Ok(())
     }
 
     fn sanitize_certificate(&mut self, certificate: &Certificate) -> DagResult<()> {
