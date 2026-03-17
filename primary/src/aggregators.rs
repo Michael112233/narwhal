d80d96642ed232a47e11a3430d04ff2221a3275c
chenlinwu2 <michaelwcl@cityu.edu.hk>
2026-03-17 20:05:37 +0800

    modify solid step judgement logic

--- primary/src/aggregators.rs
+++ primary/src/aggregators.rs
@@ -119,7 +119,7 @@ impl CertificatesAggregator {
         // Add the certificate to the appropriate list.
         if certificate.round() == self.expected_round {
             self.certificates.push(certificate.digest());
-            if current_round % committee.solid_step_length() == 0 {
+            if current_round % committee.solid_step_length() == 1 && current_round > 1 {
                 self.cert_instance.push(certificate.clone());
                 // debug!("Cert instance size: {}, certificates size: {}", self.cert_instance.len(), self.certificates.len());
             }
@@ -127,7 +127,7 @@ impl CertificatesAggregator {
         } else if certificate.round() >= weak_start && certificate.round() < self.expected_round {
             self.certificates.push(certificate.digest());
             self.weak_certificates.push(certificate.digest());
-            if current_round % committee.solid_step_length() == 0 {
+            if current_round % committee.solid_step_length() == 1 && current_round > 1 {
                 self.weight += committee.stake(&origin);
                 self.cert_instance.push(certificate.clone());
                 // debug!("Cert instance size: {}, certificates size: {}", self.cert_instance.len(), self.certificates.len());
@@ -141,7 +141,7 @@ impl CertificatesAggregator {
         );
 
         let threshold = committee.processing_threshold(current_round);
-        let is_solid_step = current_round % committee.solid_step_length() == 0 && current_round > 1;
+        let is_solid_step = current_round % committee.solid_step_length() == 1 && current_round > 1;
         debug!(
             "Advance to round {}: require weight >= {}, solid_step={})",
             current_round, threshold, is_solid_step
