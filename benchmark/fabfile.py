24276b02c2e878f71ba5ceb0c0c606dcf2d4a573
michaelwcl323 <chenlinwu2-c@my.cityu.edu.hk>
2026-03-25 21:06:53 -0600

    test

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -246,7 +246,7 @@ def cloudlab_remote(ctx, debug=True):
     }
     node_params = {
         'header_size': 1000,  # bytes
-        'max_header_delay': 200,  # ms
+        'max_header_delay': 50,  # ms
         'gc_depth': 50,  # rounds
         'sync_retry_delay': 1000,  # ms
         'sync_retry_nodes': 7,  # number of nodes
